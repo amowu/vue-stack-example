049235d0918e19d8a2caea4ff791fcf26a19b0a4
Amo Wu <amo260@gmail.com>
2016-07-01 21:39:11 +0800

    feat(router): add Hero List Page

---
+++ src/components/HeroListPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Hero List Page</h1>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import App from './App'
+import HeroListPage from './components/HeroListPage'
 
 Vue.use(VueRouter)
 
 let router = new VueRouter({
   history: true
 })
+
+router.map({
+  '/heroes': {
+    component: HeroListPage,
+  }
+})
+
 router.start(App, 'app')
