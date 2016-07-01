97acbf7a18f48e13bf59ecf10adbae9d6c76f20e
Amo Wu <amo260@gmail.com>
2016-07-01 21:36:09 +0800

    feat(router): add vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+Vue.use(VueRouter)
+
 })
+router.start(App, 'app')
