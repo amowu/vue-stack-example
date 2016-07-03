e8ceb8877f6a0f5c852c426d3d5be522f1d12d11
Amo Wu <amo260@gmail.com>
2016-07-03 15:21:57 +0800

    refactor: using <numerical-input>

--- src/components/HeroProfilePage.vue
+++ src/components/HeroProfilePage.vue
@@ -1,27 +1,58 @@
+<script>
+  import NumericalInput from './NumericalInput'
+
+  export default {
+    data () {
+      return {
+        status: {
+          str: 2,
+          int: 7,
+          agi: 9,
+          luk: 7
+        },
+        total: 25
+      }
+    },
+    computed: {
+      sum () {
+        return Object.keys(this.status)
+                     .reduce((sum, key) => sum + this.status[key], 0)
+      },
+      remain () {
+        let { total, sum } = this
+        return total - sum
+      }
+    },
+    components: {
+      'numerical-input': NumericalInput
+    }
+  }
+</script>
+
 <template>
   <div>
     <p>heroId: {{ $route.params.heroId }}</p>
     <ul>
       <li class="hero-status">
         STR
-        <input type="number" name="str" min="0" max="25" value="2">
+        <numerical-input :value="status.str"></numerical-input>
       </li>
       <li class="hero-status">
         INT
-        <input type="number" name="int" min="0" max="25" value="7">
+        <numerical-input :value="status.int"></numerical-input>
       </li>
       <li class="hero-status">
         AGI
-        <input type="number" name="agi" min="0" max="25" value="9">
+        <numerical-input :value="status.agi"></numerical-input>
       </li>
       <li class="hero-status">
         LUK
-        <input type="number" name="luk" min="0" max="25" value="7">
+        <numerical-input :value="status.luk"></numerical-input>
       </li>
     </ul>
     <div>
       剩餘點數：
-      <output name="hero-status-sum">0</output>
+      <output name="hero-status-remain">{{ remain }}</output>
     </div>
     <button name="hero-status-updated-btn">儲存</button>
   </div>
