55217911087c632dde1b4873b691c566cef5f353
Amo Wu <amo260@gmail.com>
2016-07-03 21:35:03 +0800

    style: move scripting code to top

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
+<script>
+  import Hello from './components/Hello'
+
+  export default {
+    components: {
+      Hello
+    }
+  }
+</script>
+
 <template>
   <div id="app">
     <router-view></router-view>
   </div>
 </template>
-
-<script>
-import Hello from './components/Hello'
-
-export default {
-  components: {
-    Hello
-  }
-}
-</script>
