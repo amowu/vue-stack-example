8d1356edac44c8e7b66fc1e35c90eb909c5f06e3
Amo Wu <amo260@gmail.com>
2016-07-08 03:41:40 +0800

    chore: disable HTML5 history mode

--- src/cores/router.js
+++ src/cores/router.js
@@ -7,7 +7,7 @@ import HeroProfilePage from '../pages/HeroProfilePage'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  history: true // 如果你的 server 不支援 HTML5 Histroy API，請設為 false
+  history: false // 如果你的 server 不支援 HTML5 Histroy API，請設為 false
 })
 
 router.map({
