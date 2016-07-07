9e339e57608ea67726411d35923889ec1f868524
Amo Wu <amo260@gmail.com>
2016-07-08 03:40:55 +0800

    chore: remove production server

--- src/cores/config.js
+++ src/cores/config.js
@@ -2,7 +2,7 @@ const IS_LOCAL = process.env.NODE_ENV !== 'production'
 
 const API_ENDPOINT_URL = IS_LOCAL
   ? 'http://localhost:3000' // development
-  : 'https://hahow-recruit.herokuapp.com' // production
+  : '' // production
 
 export {
   API_ENDPOINT_URL
