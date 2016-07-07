00920900f4ac7325e1f9d2cc60290d0882a0b7ae
Amo Wu <amo260@gmail.com>
2016-07-08 02:46:08 +0800

    feat: add json-server mock db
    How to run this mock server on dev-env:
    $ npm install -g json-server
    $ cd json-server
    $ json-server --watch db.json --routes routes.json

--- src/cores/config.js
+++ src/cores/config.js
@@ -1,7 +1,7 @@
 const IS_LOCAL = process.env.NODE_ENV !== 'production'
 
 const API_ENDPOINT_URL = IS_LOCAL
-  ? 'https://hahow-recruit.herokuapp.com' // development
+  ? 'http://localhost:3000' // development
   : 'https://hahow-recruit.herokuapp.com' // production
 
 export {
